82a2a855e7659ccf4c8a0a35b9a62b6d55830b04
manharjotkaur <manharjotkaur123@gmail.com>
2019-06-12 07:06:57 -0400

    added homework2

---
+++ coding/bubblesort/bubblesort.cpp
@@ -0,0 +1,41 @@
+/*
+ * bubblesort.cpp
+ *
+ *  Created on: Jun 12, 2019
+ *      Author: manharjot
+ */
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int number[5];
+	int i,j;
+	cout<<"Enter the no.";
+	for(i=0;i<=4;i++);
+	{
+		cin>>number[5];
+	}
+	for(i=0;i<=3;i++);
+	{
+		for(j=i+1;j<=5;j++);
+		{
+			int temp;
+			if(number[i]<number[j])
+			{
+				temp=number[i];
+				number[i]=number[j];
+				number[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<=4;i++);
+	{
+		cout<<"=>"<<number[i]<<endl;
+	}
+	return 0;
+}
+
+
+
+
+
