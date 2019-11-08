f543c782899da0fd037ef134bbdf274057751c2b
manharjotkaur <51168225+manharjotkaur@users.noreply.github.com>
2019-11-07 23:58:31 -0500

    Update wordcount.cpp

--- wordcount.cpp
+++ wordcount.cpp
@@ -2,9 +2,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
 using namespace std;
-
  int main()
  {
      cout<<"Please enter some text. Then Press <CTRL><D> to signal EoF:" <<endl;
